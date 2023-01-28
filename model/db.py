64bee80134efdf225ba6b631a3ad72e5de3ab5c8
KristianRincon <krisdaringon@gmail.com>
2023-01-27 23:30:21 -0500

    Conexion a base de datos

---
+++ model/db.py
@@ -0,0 +1,19 @@
+from flask_mysqldb import MySQL
+from application import app
+from MySQLdb.cursors import Cursor
+
+# Establecemos la coneccion a la base de datos
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'coltis_movie_flask'
+app.config['MYSQL_PORT'] = 3306
+mysql = MySQL(app)
+
+def execute(sql: str) -> Cursor:
+    cursor = mysql.connection.cursor()
+    cursor.execute(sql)
+    return cursor
+
+def commit() -> None:
+    mysql.connection.commit()
